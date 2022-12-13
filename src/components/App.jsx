9a5eab763457718f0558dfd412c04646e19a69d0
Serhii Malychkovych <masevik@gmail.com>
2022-12-13 20:37:25 +0200

    add localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,12 +8,7 @@ import { Section } from './Section';
 
 export class App extends Component {
   state = {
-    contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -28,6 +23,7 @@ export class App extends Component {
       alert(name + ' is already in contacts');
       return false;
     }
+
     const currentContact = {
       name,
       number,
@@ -37,7 +33,6 @@ export class App extends Component {
     this.setState(({ contacts }) => ({
       contacts: [currentContact, ...contacts],
     }));
-
     return true;
   };
 
@@ -46,6 +41,20 @@ export class App extends Component {
       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }));
   };
+
+  componentDidMount() {
+    const parsedStorage = JSON.parse(localStorage.getItem('contacts'));
+
+    if (parsedStorage !== null) {
+      this.setState({ contacts: parsedStorage });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     const { addContact, onFilterChange, deleteContact } = this;
